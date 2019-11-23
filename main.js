119bc5154e0f7d1809de073a4a2af86c7cfe8c4a
draglar <57449398+draglar@users.noreply.github.com>
2019-11-23 13:51:32 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var bt=document.getElementById('kjb');
+var par=document.querySelectorAll('.para');
+bt.addEventListener('click',function(){
+    par.innerHTML='It already started';
+    para.classList.toggle('.kjb');
+});
+
+function mout(){
+    var trns=document.getElementById('intro');
+    trns.style.transition='4s';
+}
+function emp(){
+    var eem=document.getElementById('title');
+    eem.style.transition='3s';
+}
+function drkecrt(){
+    dark.html
+}
